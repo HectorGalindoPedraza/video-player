9a9dcf0e34963a03dc4d585199c3eb0f4ecef405
hec.gph <hec.gph@gmail.com>
2021-07-12 23:49:01 -0500

    progreso del reproductor

--- js/index.js
+++ js/index.js
@@ -33,3 +33,20 @@ $forward.addEventListener("click", handleForward )
     $video.currentTime +=10
     console.log("forward", $video.currentTime)
     }
+    const  $progress = document.querySelector("#progress")
+    $video.addEventListener("loadedmetadata", handleLoaded)
+    $video.addEventListener("timeupdate", handleTimeUpdate)
+
+    function handleLoaded(){
+        $progress.max = $video.duration
+        console.log("ha cargado mi video", $video.duration)
+    }
+    function handleTimeUpdate(){
+        $progress.value = $video.currentTime
+        // console.log("tiempo actual", $video.currentTime)
+    }
+    $progress.addEventListener("input", handleInput)
+    function handleInput(){
+        $video.currentTime = $progress.value
+        console.log($progress.value)
+    }
